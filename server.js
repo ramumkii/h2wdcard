57050fb66cdaf5923e272d8222ab1e89abb7d3f9
1002627 <pyxis@sk.com>
2016-11-27 05:01:05 +0900

    webpack 설정

--- server.js
+++ server.js
@@ -1,18 +1,34 @@
 var express = require('express');
 var http = require('http');
+var path = require('path');
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
-var app = express();
+
+// Front-end works
+var webpack = require('webpack');
+var webpackMiddleware = require('webpack-dev-middleware');
+var config = require('./webpack.config.js');
 
 // Routers
 var index = require('./routers/index');
 var apis = require('./apis/index');
 
-app.set('port', process.env.PORT || 3000);
+// Environments
+var isDeveloping = process.env.NODE_ENV !== 'production';
+var port = isDeveloping ? 3000 : process.env.PORT;
+var app = express();
 
+if (isDeveloping) {
+  var compiler = webpack(config);
+  var middleware = webpackMiddleware(compiler, {
+    publicPath: config.output.publicPath,
+    noInfo: true
+  });
+  app.use(middleware);
+}
 // Middlewares
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(path.join(__dirname, 'dist')));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(methodOverride());
@@ -22,6 +38,6 @@ app.use('/', index);
 app.use('/apis', apis);
 
 // Server
-http.createServer(app).listen(app.get('port'), function() {
-  console.log('Express server listening on port ' + app.get('port'));
+http.createServer(app).listen(port, function() {
+  console.log('Express server listening on port ' + port);
 });
