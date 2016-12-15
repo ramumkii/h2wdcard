2d9d42a27b414912d3f10ef0df14ce6b539d7526
1002627 <pyxis@sk.com>
2016-12-15 18:44:33 +0900

    Add a route: region

--- apis/index.js
+++ apis/index.js
@@ -4,9 +4,11 @@ var express = require('express');
 var router = express.Router();
 
 var user = require('./user');
+var region = require('./region');
 
 module.exports = (function() {
     router.use('/user', user);
+    router.user('/region', region);
     // info 결혼식 정보
     // image 이미지 갤러리
     // board 방명록
